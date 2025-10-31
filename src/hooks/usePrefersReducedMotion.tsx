bbb3e3939dfa5fece8ae59448eda4af0acc9d2ae
Feyza Seyfi <demlikposet@gmail.com>
2025-10-31 16:18:11 +0300

    Create usePrefersReducedMotion hook

---
+++ src/hooks/usePrefersReducedMotion.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+const QUERY = '(prefers-reduced-motion: reduce)';
+export default function usePrefersReducedMotion() {
+  const [prefersReducedMotion, setPrefersReducedMotion] = useState(true);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(QUERY);
+
+    const id = requestAnimationFrame(() => {
+      setPrefersReducedMotion(mediaQuery.matches);
+    });
+
+    const listener = (event: MediaQueryListEvent) => {
+      setPrefersReducedMotion(event.matches);
+    };
+    mediaQuery.addEventListener('change', listener);
+
+    return () => {
+      mediaQuery.removeEventListener('change', listener);
+      cancelAnimationFrame(id);
+    };
+  }, []);
+
+  return prefersReducedMotion;
+}
