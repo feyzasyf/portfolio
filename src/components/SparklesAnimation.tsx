da1ac767008f5667fa802c24fdf3165abe34b2ba
Feyza Seyfi <demlikposet@gmail.com>
2025-11-02 15:30:46 +0300

    Update animation color and particle size

--- src/components/SparklesAnimation.tsx
+++ src/components/SparklesAnimation.tsx
@@ -15,10 +15,10 @@ export default function SparklesAnimation() {
         id='tsparticlescolorful'
         background='transparent'
         minSize={0.6}
-        maxSize={1.4}
-        particleDensity={50}
+        maxSize={2.4}
+        particleDensity={30}
         className='w-full h-full'
-        particleColor={theme === 'dark' ? '#00ff00' : '#ff0000'}
+        particleColor={theme === 'dark' ? '#00ff00' : '#000000'}
         speed={0.5}
       />
     </div>
