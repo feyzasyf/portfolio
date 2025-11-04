02b2e40c95b06e14e651ec412f4eac0b6c529804
Feyza Seyfi <demlikposet@gmail.com>
2025-11-05 00:08:40 +0300

    Install react-hot-toast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import About from '@/app/sections/About';
 import Landing from './sections/Landing';
 import Resume from './sections/Resume';
+import ThanksMessage from './sections/ThanksMessage';
+import { Toaster } from 'react-hot-toast';
 
 export default function Home() {
   return (
     <div className='flex flex-col items-center w-full font-sans'>
+      <Toaster position='bottom-center' />
       <Landing />
       <About />
       <Resume />
+      <ThanksMessage />
     </div>
   );
 }
