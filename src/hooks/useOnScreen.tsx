a872c783f213b3899c0bf3067d6144d060919af8
Feyza Seyfi <demlikposet@gmail.com>
2025-11-05 00:09:09 +0300

    Create useOnScreen hook to check if element is visible

---
+++ src/hooks/useOnScreen.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+export default function useOnScreen() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const element = document.getElementById('end');
+      if (element) {
+        const rect = element.getBoundingClientRect();
+        setIsVisible(rect.top >= 0 && rect.bottom <= window.innerHeight);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return isVisible;
+}
