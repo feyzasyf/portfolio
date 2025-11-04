d24755f0516067d39ae382915aca70fd46936e74
Feyza Seyfi <demlikposet@gmail.com>
2025-11-04 18:53:15 +0300

    Add image to footer

--- src/app/sections/Footer.tsx
+++ src/app/sections/Footer.tsx
@@ -1,14 +1,21 @@
 import SocialLinks from '@/components/SocialLinks';
-import React from 'react';
+import Image from 'next/image';
 
 export default function Footer() {
   return (
     <section className='w-full pb-3 text-sm '>
       <div className='mb-4 bg-linear-to-r from-transparent via-green-800 to-transparent h-px w-full' />
       <div className='wrapper pb-4 '>
-        <p className='hidden sm:block sm:basis-1/3'>
-          No AI has been involved in making of this website
-        </p>
+        <div className='hidden sm:flex flex-row items-center gap-1 sm:basis-1/3'>
+          <p>Pixel art by me</p>
+          <Image
+            src='/heart.png'
+            alt='Pixel Heart'
+            width={30}
+            height={30}
+            unoptimized
+          />
+        </div>
 
         <div className='basis-1/3 flex justify-center'>
           <SocialLinks />
